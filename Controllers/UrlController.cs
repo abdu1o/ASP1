f41f36ca1eb1434551570de47f2e626a425e1762
abdu1o <a.kapara2006@gmail.com>
2024-10-02 18:30:09 +0300

    Add project files.

---
+++ Controllers/UrlController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP1.Controllers
+{
+    public class UrlController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.CurrentDate = DateTime.Now;
+            return View();
+        }
+    }
+}
